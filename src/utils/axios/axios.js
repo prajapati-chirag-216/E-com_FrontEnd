0e5f6525475d02e2eed357a2fa55f9590bf6bc7a
nisha soni <yashusoni003@gmail.com>
2023-06-13 13:37:29 +0530

    qwe

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 // const BACKAND_DOMAIN = "http://localhost:8000";
-const BACKAND_DOMAIN = "https://shopzee-back.onrender.com";
+// const BACKAND_DOMAIN = "https://shopzee-back.onrender.com";
+const BACKAND_DOMAIN = "https://shopzee-back-production.up.railway.app";
+
 export default axios.create({
   baseURL: BACKAND_DOMAIN,
 });
