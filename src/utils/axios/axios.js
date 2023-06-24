f4a8c61cd585bfe707713b16be54fb8256751224
nisha soni <yashusoni003@gmail.com>
2023-06-25 00:53:11 +0530

    we

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BACKAND_DOMAIN = "http://192.168.43.125:8000";
-// const BACKAND_DOMAIN = "https://www.shopzeee.shop";
+// const BACKAND_DOMAIN = "http://192.168.43.125:8000";
+const BACKAND_DOMAIN = "https://www.shopzeee.shop";
 
 export default axios.create({
   baseURL: BACKAND_DOMAIN,
