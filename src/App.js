7b4802492b0c9b70f140cf251034dc8dc73da155
nisha soni <yashusoni003@gmail.com>
2023-07-16 17:37:24 +0530

    ew

--- src/App.js
+++ src/App.js
@@ -27,35 +27,50 @@ import PrivateRoutes from "./routes/PrivateRoutes";
 import Error from "./pages/Error/Error";
 import Success from "./pages/Success/Success";
 import Wrapper from "./components/Dekstop/UI/Wrapper";
-const  Layout = lazy(()=>import( "./components/Dekstop/UI/Layout"))
-const SignIn = lazy(()=>import("./pages/SignIn"))
-const ProductsController= lazy(()=>import( "./pages/Products/ProductsController"))
-const ViewProductsController= lazy(()=>import( "./pages/ProdutsView/ProductsViewController"))
-const Signup= lazy(()=>import( "./pages/Signup"))
-const Checkout= lazy(()=>import( "./pages/Checkout/Checkout"))
-const MyCart= lazy(()=>import( "./components/Cart/MyCart/mycart.component"))
-const ContactUs= lazy(()=>import( "./components/ContactUs/contactUs.component"))
-const AboutUs= lazy(()=>import( "./components/AboutUs/AboutUs.component"))
-const UserProfile= lazy(()=>import( "./components/UserProfile/UserProfile.component"))
-const Information = lazy(()=>import( "./components/Checkout/Information/Information"))
-const  Shipping =  lazy(()=>import("./components/Checkout/Shipping/Shipping"))
-const Payment =  lazy(()=>import("./components/Checkout/Paymet/Payment"))
-const  OrderStatus = lazy(()=> import("./components/UserProfile/MyOrders/OrderStatus"))
-const Dashboard = lazy(() => import('./pages/Deshboard/Deshboard'))
+// const  Layout = lazy(()=>import( "./components/Dekstop/UI/Layout"))
+// const SignIn = lazy(()=>import("./pages/SignIn"))
+// const ProductsController= lazy(()=>import( "./pages/Products/ProductsController"))
+// const ViewProductsController= lazy(()=>import( "./pages/ProdutsView/ProductsViewController"))
+// const Signup= lazy(()=>import( "./pages/Signup"))
+// const Checkout= lazy(()=>import( "./pages/Checkout/Checkout"))
+// const MyCart= lazy(()=>import( "./components/Cart/MyCart/mycart.component"))
+// const ContactUs= lazy(()=>import( "./components/ContactUs/contactUs.component"))
+// const AboutUs= lazy(()=>import( "./components/AboutUs/AboutUs.component"))
+// const UserProfile= lazy(()=>import( "./components/UserProfile/UserProfile.component"))
+// const Information = lazy(()=>import( "./components/Checkout/Information/Information"))
+// const  Shipping =  lazy(()=>import("./components/Checkout/Shipping/Shipping"))
+// const Payment =  lazy(()=>import("./components/Checkout/Paymet/Payment"))
+// const  OrderStatus = lazy(()=> import("./components/UserProfile/MyOrders/OrderStatus"))
+// const Dashboard = lazy(() => import('./pages/Deshboard/Deshboard'))
+import  Layout from  "./components/Dekstop/UI/Layout"
+import SignIn  from "./pages/SignIn"
+import ProductsController from "./pages/Products/ProductsController"
+import ViewProductsController from  "./pages/ProdutsView/ProductsViewController"
+import Signup  from "./pages/Signup"
+import Checkout from "./pages/Checkout/Checkout"
+import MyCart from "./components/Cart/MyCart/mycart.component"
+import ContactUs from "./components/ContactUs/contactUs.component"
+import AboutUs from "./components/AboutUs/AboutUs.component"
+import UserProfile from "./components/UserProfile/UserProfile.component"
+import Information from  "./components/Checkout/Information/Information"
+import  Shipping from"./components/Checkout/Shipping/Shipping"
+import Payment from "./components/Checkout/Paymet/Payment"
+import  OrderStatus from "./components/UserProfile/MyOrders/OrderStatus"
+import Dashboard from './pages/Deshboard/Deshboard'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Wrapper />} errorElement={<Error />}>
       <Route path="/signup" element={
-        <Suspense fallback={<LoadingSpinner />}>
+        // <Suspense fallback={<LoadingSpinner />}>
+        // </Suspense>
           <Signup />
-        </Suspense>
       } action={signupAction} />
       <Route path="/login" element={
-        <Suspense fallback={<LoadingSpinner />}>
+        // <Suspense fallback={<LoadingSpinner />}>
+        // </Suspense>
           <SignIn />
-        </Suspense>
       } action={signinAction} />
       <Route
         path="/forgotPassword"
@@ -81,58 +96,58 @@ const router = createBrowserRouter(
         loader={profileLoader}
       >
         <Route path="/checkout/" element={
-          <Suspense fallback={<LoadingSpinner />}>
+          // <Suspense fallback={<LoadingSpinner />}>
+          // </Suspense>
             <Checkout />
-          </Suspense>
         }>
           <Route index element={
-            <Suspense fallback={<LoadingSpinner />}>
+            // <Suspense fallback={<LoadingSpinner />}>
+            // </Suspense>
               <Information />
-            </Suspense>
           } />
           <Route path="shipping" element={
-            <Suspense fallback={<LoadingSpinner />}>
+            // <Suspense fallback={<LoadingSpinner />}>
+            // </Suspense>
               <Shipping />
-            </Suspense>
           } />
           <Route path="payment" element={
-            <Suspense fallback={<LoadingSpinner />}>
+            // <Suspense fallback={<LoadingSpinner />}>
+            // </Suspense>
               <Payment />
-            </Suspense>
           } />
         </Route>
       </Route>
 
       <Route element={
-        <Suspense fallback={<LoadingSpinner />}>
+        // <Suspense fallback={<LoadingSpinner />}>
+        // </Suspense>
           <Layout />
-        </Suspense>
       }>
         <Route index element={
-          <Suspense fallback={<LoadingSpinner />}>
+          // <Suspense fallback={<LoadingSpinner />}>
+          // </Suspense>
             <Navigate to="/home" />
-          </Suspense>
         } />
         <Route
           action={messageAction}
           path="/contactus"
           element={
-            <Suspense fallback={<LoadingSpinner />}>
+            // <Suspense fallback={<LoadingSpinner />}>
+            // </Suspense>
               <ContactUs />
-            </Suspense>
 
           }
         />
         <Route path="/aboutus" element={
-          <Suspense fallback={<LoadingSpinner />}>
+          // <Suspense fallback={<LoadingSpinner />}>
+          // </Suspense>
             <AboutUs />
-          </Suspense>
         } />
 
         <Route path="/home" element={
-          <Suspense fallback={<LoadingSpinner />}>
+          // <Suspense fallback={<LoadingSpinner />}>
+          // </Suspense>
             <Dashboard />
-          </Suspense>
         } loader={categoryLoader} />
 
         <Route
@@ -145,39 +160,39 @@ const router = createBrowserRouter(
           loader={profileLoader}
         >
           <Route path="/myProfile" element={
-            <Suspense fallback={<LoadingSpinner />}>
+            // <Suspense fallback={<LoadingSpinner />}>
+            // </Suspense>
               <UserProfile />
-            </Suspense>
           } />
         </Route>
         <Route
           path="/product/:id"
           element={
-            <Suspense fallback={<LoadingSpinner />}>
+            // <Suspense fallback={<LoadingSpinner />}>
+            // </Suspense>
               <ProductsController />
-            </Suspense>
           }
           loader={productLoader}
         />
         <Route
           path="/viewproduct/:id"
           element={
-            <Suspense fallback={<LoadingSpinner />}>
+            // <Suspense fallback={<LoadingSpinner />}>
+            // </Suspense>
               <ViewProductsController />
-            </Suspense>
           }
           action={reviewAction}
           loader={productViewLoader}
         />
         <Route path="/cart" element={
-          <Suspense fallback={<LoadingSpinner />}>
+          // <Suspense fallback={<LoadingSpinner />}>
+          // </Suspense>
             <MyCart />
-          </Suspense>
         } />
         <Route path="/orderStatus/:id" element={
-          <Suspense fallback={<LoadingSpinner />}>
+          // <Suspense fallback={<LoadingSpinner />}>
+          // </Suspense>
             <OrderStatus />
-          </Suspense>
         } />
       </Route>
     </Route>
